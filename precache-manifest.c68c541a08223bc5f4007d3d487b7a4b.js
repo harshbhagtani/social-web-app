a77b2c8f659ab54ee3449fd7519aca8f2bfd07a1
harshbhagtani1 <harsh.bhagtani1@gmail.com>
2022-06-10 11:24:09 +0530

    Updates

---
+++ precache-manifest.c68c541a08223bc5f4007d3d487b7a4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "873fecc31cd43ed92cc3940eb1c6da42",
+    "url": "/social-web-app/index.html"
+  },
+  {
+    "revision": "719cb6d71cd664824829",
+    "url": "/social-web-app/static/css/main.54cbebb7.chunk.css"
+  },
+  {
+    "revision": "96cedcd31159f2fdc7f0",
+    "url": "/social-web-app/static/js/2.41f1c7f7.chunk.js"
+  },
+  {
+    "revision": "e9256ddc4faa5439b55717202e329355",
+    "url": "/social-web-app/static/js/2.41f1c7f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "719cb6d71cd664824829",
+    "url": "/social-web-app/static/js/main.c6779a7a.chunk.js"
+  },
+  {
+    "revision": "3a3c92d26458bacb60d4",
+    "url": "/social-web-app/static/js/runtime-main.811d1d86.js"
+  }
+]);
